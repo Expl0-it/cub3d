62de9f89cdb216efc74ca6e9f50a62ad46d43a1f
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-01-19 14:24:09 +0100

    added cub3d.h + header files

---
+++ includes/cub3d.h
@@ -0,0 +1,19 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft.h"
+# include "mlx.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+
+#endif
