069631386763ee08fa4ece3958164f7a3c333eef
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-02-25 19:45:03 +0100

    handled rename in init

--- src/init/init.c
+++ src/init/init.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:10:03 by mamichal          #+#    #+#             */
-/*   Updated: 2025/02/25 19:18:15 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/02/25 19:24:00 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 static bool init_mlx(t_game *game)
 {
-	game->mlx.p_mlx = mlx_init();
-	if (NULL == game->mlx.p_mlx)
+	game->mlx_s.p_mlx = mlx_init();
+	if (NULL == game->mlx_s.p_mlx)
 		return (false);
-	game->mlx.wnd = mlx_new_window(game->mlx.p_mlx, WIDTH, HEIGHT, "cub3d");
-	if (NULL == game->mlx.wnd)
+	game->mlx_s.wnd = mlx_new_window(game->mlx_s.p_mlx, WIDTH, HEIGHT, "cub3d");
+	if (NULL == game->mlx_s.wnd)
 		return (false);
-	game->mlx.img = mlx_new_image(game->mlx.p_mlx, WIDTH, HEIGHT);
-	if (NULL == game->mlx.img)
+	game->mlx_s.img = mlx_new_image(game->mlx_s.p_mlx, WIDTH, HEIGHT);
+	if (NULL == game->mlx_s.img)
 		return (false);
-	game->mlx.data = mlx_get_data_addr(game->mlx.img, &game->mlx.init.bits, \
-		&game->mlx.init.len, &game->mlx.init.endian);
-	if (NULL == game->mlx.data)
+	game->mlx_s.data = mlx_get_data_addr(game->mlx_s.img, &game->mlx_s.init.bits, \
+		&game->mlx_s.init.len, &game->mlx_s.init.endian);
+	if (NULL == game->mlx_s.data)
 		return (false);
 	return (true);
 }
