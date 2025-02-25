47441380f758d397a5a12df768001569cb54d087
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-02-25 20:04:24 +0100

    added skeleton to input and update

--- src/run/input.c
+++ src/run/input.c
@@ -6,13 +6,18 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:25:14 by mamichal          #+#    #+#             */
-/*   Updated: 2025/02/25 19:38:33 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/02/25 19:58:03 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	input(int key, void *game)
+int	input(int key, void *game_cast)
 {
-	(t_game *)game;
+	t_game	*game;
+
+	game = (t_game *)game_cast;
+	(void)game;
+	(void)key;
+	return (OK);
 }
--- src/run/update.c
+++ src/run/update.c
@@ -6,13 +6,17 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:25:14 by mamichal          #+#    #+#             */
-/*   Updated: 2025/02/25 19:36:28 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/02/25 19:59:54 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	update(void *game)
+int	update(void *game_cast)
 {
-	(t_game *)game;
+	t_game	*game;
+
+	game = (t_game *)game_cast;
+	(void)game;
+	return (OK);
 }
