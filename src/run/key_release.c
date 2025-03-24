cca5da217f457204ce662c03ba60f348e4e3f39e
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-03-24 16:06:48 +0100

    handling key_release

--- src/run/key_release.c
+++ src/run/key_release.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:25:14 by mamichal          #+#    #+#             */
-/*   Updated: 2025/03/24 10:03:47 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/03/24 16:06:28 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 int	key_release(int key, t_game *game)
 {
-	(void)game;
-	(void)key;
+	if (XK_W == key)
+		game->player.keys.key_up = false;
+	if (XK_S == key)
+		game->player.keys.key_down = false;
+	if (XK_A == key)
+		game->player.keys.key_left = false;
+	if (XK_D == key)
+		game->player.keys.key_right = false;
+	if (XK_Left == key)
+		game->player.keys.rotate_left = false;
+	if (XK_Right == key)
+		game->player.keys.rotate_right = false;
 	return (OK);
 }
