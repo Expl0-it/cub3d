f97624783d3e703322891d04074237fe0977711c
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-03-24 16:09:48 +0100

    added rotate and move to update fn

--- src/run/update.c
+++ src/run/update.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:25:14 by mamichal          #+#    #+#             */
-/*   Updated: 2025/03/04 10:13:12 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/03/24 16:09:20 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int	update(t_game *game)
 {
-	(void)game;
+	t_player	*player;
+
+	player = &game->player;
+	rotate_player(player);
+	move_player(game, player);
 	return (OK);
 }
