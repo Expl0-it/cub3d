b41cb47cc6a57d19f9a63760e9a53b99827e1be2
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-03-24 15:52:50 +0100

    added a fn to check for collision in radius

--- src/run/movement.c
+++ src/run/movement.c
@@ -24,3 +24,29 @@ void	rotate_player(t_player *player)
 		player->angle = 2 * M_PI;
 }
 
+static bool	touch(float px, float py, t_game *game)
+{
+	int	x1;
+	int	y1;
+	int	mapx;
+	int	mapy;
+
+	x1 = (int)((px + COLLISION_RADIUS) / BLOCK);
+	y1 = (int)((py + COLLISION_RADIUS) / BLOCK);
+	mapy = (int)((py - COLLISION_RADIUS) / BLOCK);
+	while (mapy <= y1)
+	{
+		mapx = (int)((px - COLLISION_RADIUS) / BLOCK);
+		while (mapx <= x1)
+		{
+			if (mapy < 0 || game->map[mapy] == NULL
+				|| game->map[mapy][mapx] == '\0'
+				|| game->map[mapy][mapx] == '1')
+				return (true);
+			mapx++;
+		}
+		mapy++;
+	}
+	return (false);
+}
+
