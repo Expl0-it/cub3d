ac008164b6b739b547d606ec3f1582c242fabf13
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-03-31 22:00:54 +0200

    added calc_delta_dist

--- src/raycast/calc.c
+++ src/raycast/calc.c
@@ -7,3 +7,15 @@ static void	init_ray(t_ray *ray, t_player *player, float ray_angle)
 	ray->mapy = player->y / BLOCK;
 }
 
+static void	calc_deta_dist(t_ray *ray)
+{
+	if (0 == ray->raydirx)
+		ray->deltadistx = 1e30f;
+	else
+		ray->deltadistx = fabs(1.0f / ray->raydirx);
+	if (0 == ray->raydiry)
+		ray->deltadisty = 1e30f;
+	else
+		ray->deltadisty = fabs(1.0f / ray->raydiry);
+}
+
