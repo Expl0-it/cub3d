cef27a2a6c2c42cde035721cc97315418a9ae93c
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-01 10:41:21 +0200

    added draw column fn

--- src/run/draw.c
+++ src/run/draw.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 11:07:17 by mamichal          #+#    #+#             */
-/*   Updated: 2025/03/31 11:49:13 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/03/31 22:54:37 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,27 @@ void	draw_floor_ceil(t_game *game)
 			pixel_put(game->mlx_s, x, y, game->colors.floor_c);
 			x++;
 		}
+		y++;
+	}
+}
+
+void	draw_column(t_game *game, t_texture *texture, int col, t_line *line)
+{
+	float	step;
+	float	tex_pos;
+	int		y;
+	int		tex_y;
+	int		color;
+
+	step = (float)texture->height / (float)line->height;
+	tex_pos = (line->start_y - HEIGHT / 2 + line->height / 2) * step;
+	y = line->start_y;
+	while (y < line->end_y)
+	{
+		tex_y = (int)tex_pos & (texture->height - 1);
+		color = *(int *)(texture->data + (tex_y * texture->mlx_val.line_len) + line->texture_x * (texture->mlx_val.bpp / 8));
+		pixel_put(game->mlx_s->p_mlx, col, y, color);
+		tex_pos += step;
 		y++;
 	}
 }
