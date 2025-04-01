9bf76d355231218381b3336fd90615a9253cb3eb
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-01 10:40:41 +0200

    added compute_distance co see how for the line is from the camera

--- src/raycast/dda.c
+++ src/raycast/dda.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 21:44:27 by mamichal          #+#    #+#             */
-/*   Updated: 2025/03/31 21:54:05 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/03/31 22:31:07 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,26 @@ void	dda(t_game *game, t_ray *ray)
 		if (game->map[ray->mapy][ray->mapx] == '1')
 			ray->hit = true;
 	}
+}
+
+float	compute_distance(t_player *player, t_ray *ray, float ray_angle)
+{
+	float	perpwalldist;
+	float	dist_in_pix;
+	float	angle_diff;
+
+	if (ray->side == SIDE)
+	{
+		perpwalldist = (ray->mapx - (player->x / BLOCK)
+			+ (1 - ray->stepx) / 2.0f) / ray->raydirx;
+	}
+	else
+	{
+		perpwalldist = (ray->mapy - (player->y / BLOCK)
+			+ (1 - ray->stepy) / 2.0f) / ray->raydiry;
+	}
+	dist_in_pix = perpwalldist * BLOCK;
+	angle_diff = ray_angle - player->angle;
+	dist_in_pix *= cosf(angle_diff);
+	return (dist_in_pix);
 }
