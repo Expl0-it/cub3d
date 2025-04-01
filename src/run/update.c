23dc38c0f27d753d9c80e4fa962ddef9d5fd6763
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-01 10:43:11 +0200

    added recent fn to update loop

--- src/run/update.c
+++ src/run/update.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:25:14 by mamichal          #+#    #+#             */
-/*   Updated: 2025/03/24 16:09:20 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/03/31 22:55:56 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ int	update(t_game *game)
 	player = &game->player;
 	rotate_player(player);
 	move_player(game, player);
+	clear_image(game);
+	draw_floor_ceil(game);
+	raycast(game, player);
 	return (OK);
 }
